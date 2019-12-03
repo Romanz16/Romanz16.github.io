c1e04783f2eeae5c7c20df770ebb4154088b1c7e
Romanz16 <zhyshkovych16@gmail.com>
2019-12-03 19:40:58 +0200

    expand shrink

--- script.js
+++ script.js
@@ -1,15 +1,37 @@
 let getSel = sel => document.querySelector(sel);
+let view = 'shrink';
+
 getSel('.btnSubmit').onclick = function () {
     alert('Something');
 }
-getSel('.expand').onclick = function () {
-    getSel('.wrapper1').style.display = 'none';
-    getSel('.wrapper2').style.display = 'flex';
-    let textComment = getSel('#textComment').value;
-    getSel('.contentShrink').textContent = textComment;
+getSel('#expand').onclick = function () {
+    if (view === 'shrink') {
+        getSel('.wrapper1').style.backgroundColor = 'rgb(51, 153, 102)';
+        getSel('.header').style.display = 'none';
+        getSel('#content').className = 'contentShrink';
+        getSel('#textComment').style.height = '100%';
+        getSel('#textComment').style.margin = '0';
+        getSel('.btnSubmit').style.display = 'none';
+        getSel('.titleQuestion').style.display = 'none';
+        getSel('.expand > img').style.src = 'image/shrink.png';
+        this.innerHTML = '<img src="image/shrink.png" alt=""> SHRINK';
+        this.classList.add('shrink');
+        view = 'expand';
+    }
+    else {
+        getSel('.wrapper1').style.backgroundColor = 'transparent';
+        getSel('.header').style.display = 'block';
+        getSel('#content').className = 'contentExpand';
+        getSel('#textComment').style.height = '150px';
+        getSel('#textComment').style.margin = '9px 0';
+        getSel('#textComment').style.backgroundColor = 'white';
+        getSel('.btnSubmit').style.display = 'block';
+        getSel('.titleQuestion').style.display = 'block';
+        this.innerHTML = '<img src="image/expand.png" alt=""> EXPAND';
+        this.classList.remove('shrink');
+        view = 'shrink';
+    }
+
 }
 
-getSel('.shrink').onclick = function () {
-    getSel('.wrapper2').style.display = 'none';
-    getSel('.wrapper1').style.display = 'flex';
-}
+
